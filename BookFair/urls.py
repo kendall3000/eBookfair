84590583f74d2e7a53c869e672fb13afd7fafcc5
Kendall <kendallramos03@gmail.com>
2023-11-16 17:49:33 -0600

    kendall

--- BookFair/urls.py
+++ BookFair/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from .views import signup_profile
 
 urlpatterns = [
-    path("", views.home, name ="home")
-    #path("home", views.home, name ="home")
+    path("", views.home, name="home"),
+    #path("home", views.home, name="home"),
+    path('signup-profile/', signup_profile, name='signup_profile')
 ]
