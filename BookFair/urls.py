14cfae691412fb57783008dad24052c4fb77acaf
Rodrigo Davila Castillo <26052518+gataria@users.noreply.github.com>
2023-11-18 13:04:52 -0600

    Cleaned up urls file

--- BookFair/urls.py
+++ BookFair/urls.py
@@ -3,7 +3,8 @@ from . import views
 from .views import signup_profile
 
 urlpatterns = [
+    # Home page
     path("", views.home, name="home"),
-    #path("home", views.home, name="home"),
+    # Profile signup page
     path('signup-profile/', signup_profile, name='signup_profile')
 ]
