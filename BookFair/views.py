f298e33d443c1270b047763968a1868f648815c8
Rodrigo Davila Castillo <26052518+gataria@users.noreply.github.com>
2023-11-18 13:01:02 -0600

    Renamed folder, moved home.html file

--- BookFair/views.py
+++ BookFair/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def home(request):
-    return render(request, "home.html")
+    return render(request, "BookFair/home.html")
 
 def signup_profile(request):
     return render(request, 'BookFair/signup_profile.html')
