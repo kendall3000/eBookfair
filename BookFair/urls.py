7dfdc15153cb7a1f0aec0062ba5b773b8944a2e5
Rodrigo Davila Castillo <26052518+gataria@users.noreply.github.com>
2023-11-18 13:20:19 -0600

    Testing category page rendering

--- BookFair/urls.py
+++ BookFair/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from . import views
 from .views import signup_profile
+from .views import category
 
 urlpatterns = [
     # Home page
     path("", views.home, name="home"),
+    # Category page
+    path("category/<int:cat_id>/", category, name="category")
     # Profile signup page
     path('signup-profile/', signup_profile, name='signup_profile')
 ]
--- BookFair/views.py
+++ BookFair/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, get_object_or_404
+
+from models.models import Category
 
 # Create your views here.
 def home(request):
     return render(request, "BookFair/home.html")
+
+def category(request, cat_id):
+    req_category = get_object_or_404(Category, pk=cat_id)
+
+    return render(request, "BookFair/category.html", {"category": req_category})
 
 def signup_profile(request):
     return render(request, 'BookFair/signup_profile.html')
