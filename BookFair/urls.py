8b46d88a2c6535269651f212d24d2ca62190c621
Rodrigo Davila Castillo <26052518+gataria@users.noreply.github.com>
2023-11-18 13:35:44 -0600

    It was just a missing comma in urlpatterns

--- BookFair/urls.py
+++ BookFair/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     # Home page
     path("", views.home, name="home"),
     # Category page
-    path("<int:cat_id>/", views.category, name="category")
+    path("category/<int:cat_id>/", views.category, name="category"),
     # Profile signup page
     path('signup-profile/', signup_profile, name='signup_profile')
 ]
