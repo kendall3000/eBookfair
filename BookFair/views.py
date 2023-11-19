8e5fa1fe38335d735f5d41b57b4d91a0c8adee4b
Rodrigo Davila Castillo <26052518+gataria@users.noreply.github.com>
2023-11-18 22:58:19 -0600

    Added sorting options, changed styling

--- BookFair/views.py
+++ BookFair/views.py
@@ -28,6 +28,10 @@ def category(request, cat_id):
             cat_products_sorted = cat_products.order_by('prod_price')
         case "price-hl":
             cat_products_sorted = cat_products.order_by('-prod_price')
+        case "stock-lh":
+            cat_products_sorted = cat_products.order_by('prod-stock')
+        case "stock-hl":
+            cat_products_sorted = cat_products.order_by('-prod-stock')
         case _:
             cat_products_sorted = cat_products.order_by('prod_id').all()
 
