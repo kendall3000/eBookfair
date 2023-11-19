a66ba6241d50e73bc43e9124211665770d8c2b79
Rodrigo Davila Castillo <26052518+gataria@users.noreply.github.com>
2023-11-19 16:11:57 -0600

    Quick fix when referring to product model

--- BookFair/admin.py
+++ BookFair/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 import models.models as m
 
 # Register your models here.
-@admin.register(Product)
+@admin.register(m.Product)
 class ProductAdmin(admin.ModelAdmin):
     pass
