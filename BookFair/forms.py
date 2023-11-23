f9e994c24fa2cb528474d3efa3c54cb1b150467d
Rodrigo Davila Castillo <26052518+gataria@users.noreply.github.com>
2023-11-23 01:21:12 -0600

    Adding empty value for choices

--- BookFair/forms.py
+++ BookFair/forms.py
@@ -28,11 +28,13 @@ class SearchBox(forms.Form):
         #     "In stock, high to low"
         #     ]),
         choices = [
+            ("", "Sort by"),
             ("name", "Name"),
             ("price-lh", "Price, low to high"),
             ("price-hl", "Price, high to low"),
             ("stock-lh", "In stock, low to high"),
             ("stock-hl", "In stock, high to low")
         ],
+        default = "",
         label = "Sort by:"
     )
