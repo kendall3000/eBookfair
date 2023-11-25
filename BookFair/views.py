faf08878f6864967428c2236b1874c1addb9423b
Rodrigo Davila Castillo <26052518+gataria@users.noreply.github.com>
2023-11-25 03:32:20 -0600

    Importing UserCreationForm back to views.py

--- BookFair/views.py
+++ BookFair/views.py
@@ -1,7 +1,7 @@
 # Django packages
 from django.shortcuts import render, HttpResponse, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, authenticate
 from django.contrib import messages
 from django.db.models import Q
