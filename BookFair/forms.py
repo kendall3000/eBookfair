d8dbdcf1b6c344ced5e39a5d71ee57a07b55e0b3
Rodrigo Davila Castillo <26052518+gataria@users.noreply.github.com>
2023-11-26 00:18:34 -0600

    Importing ModelForm into forms.py

--- BookFair/forms.py
+++ BookFair/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.forms import ModelForm
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from BookFair.models import Customer
