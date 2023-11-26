f705929684ccd5b3c7ed105d8382a2206f4306f9
Rodrigo Davila Castillo <26052518+gataria@users.noreply.github.com>
2023-11-26 15:42:25 -0600

    Attempting to import LoginView correctly

--- BookFair/views.py
+++ BookFair/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, HttpResponse, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth import LoginView, login, authenticate
+from django.contrib.auth import views, login, authenticate
 from django.contrib import messages
 from django.db.models import Q
 
@@ -121,7 +121,7 @@ def signup_profile(request):
 # 'create_account_form': create_account_form,
 
 # Login
-def login(LoginView):
+def login(views.LoginView):
 
     def get_success_url(self):
         return HttpResponseRedirect("/signup-profile/") # FIXME: change to profile/ once you get that figured out
