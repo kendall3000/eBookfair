18bf0147643c4a4dec4bf98f47910878133b801e
Rodrigo Davila Castillo <26052518+gataria@users.noreply.github.com>
2023-11-26 16:19:12 -0600

    Explicitly use data= for AuthenticationForm()

--- BookFair/views.py
+++ BookFair/views.py
@@ -135,7 +135,7 @@ class login(LoginView):
         return render(request, self.template_name, {"form": form})
 
     def post(self, request, *args, **kwargs):
-        form = self.form_class(request.POST)
+        form = self.form_class(data=request.POST)
         if form.is_valid():
             messages.success(request, "Form submitted successfully.")
         else:
