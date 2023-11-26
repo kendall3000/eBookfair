ff13935083e7cf1aa3f62194b986611f77c946e0
Rodrigo Davila Castillo <26052518+gataria@users.noreply.github.com>
2023-11-26 16:42:55 -0600

    Attempt to override post() method

--- BookFair/views.py
+++ BookFair/views.py
@@ -131,6 +131,18 @@ class login(LoginView):
     def get_success_url(self):
         return reverse(signup_profile) # FIXME: change to profile/ once you get that figured out
 
+
+    def post(self, request, *args, **kwargs):
+        """
+        Handle POST requests: instantiate a form instance with the passed
+        POST variables and then check if it's valid.
+        """
+        form = self.get_form()
+        if form.is_valid():
+            return self.form_valid(form)
+        else:
+            messages.error(request, "Invalid login. Check your username and password.")
+            return self.form_invalid(form)
     # def get(self, request, *args, **kwargs):
     #     form = self.form_class()
     #     return render(request, self.template_name, {"form": form})
