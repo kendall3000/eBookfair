879ad8b1a9979da328b1b2886be3567d3552dfe4
Rodrigo Davila Castillo <26052518+gataria@users.noreply.github.com>
2023-11-25 21:03:34 -0600

    Checking if form is valid CustomUserCreationForm

--- BookFair/views.py
+++ BookFair/views.py
@@ -86,7 +86,7 @@ def signup_profile(request):
     login_account_form = AuthenticationForm()
 
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
