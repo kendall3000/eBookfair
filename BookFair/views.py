dd8e257c79617f6468f66641efd5454b03c12223
Rodrigo Davila Castillo <26052518+gataria@users.noreply.github.com>
2023-11-25 23:04:52 -0600

    Import httpresponseredirect

--- BookFair/views.py
+++ BookFair/views.py
@@ -1,5 +1,6 @@
 # Django packages
 from django.shortcuts import render, HttpResponse, get_object_or_404
+from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, authenticate
