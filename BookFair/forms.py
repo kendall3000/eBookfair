02586617f11d088128aa3018ed2e8c8fd7a9faf7
Rodrigo Davila Castillo <26052518+gataria@users.noreply.github.com>
2023-11-26 00:02:11 -0600

    Importing Customer to forms.py

--- BookFair/forms.py
+++ BookFair/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from BookFair.models import Customer
 
 class SearchBoxNav(forms.Form):
     q = forms.CharField(
